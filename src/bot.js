f4de35155f3e9b73634647fa1a34fe55d1c78102
ZeKuSx <81664702+ZeKuSx@users.noreply.github.com>
2021-04-01 09:56:47 +0200

    ban work #!

--- src/bot.js
+++ src/bot.js
@@ -59,10 +59,20 @@ command(client, 'ban', message =>{
 
     const {member, mentions} = message
 
+    const tag = `<@${member.id}`
+
     if(member.hasPermission('ADMINISTRATOR')||
     member.hasPermission('BAN_MEMBERS')
      ){
-        console.log('works')
+      const target = mentions.users.first()
+      if(target){
+        const targetMember = message.guild.member.cache.get(target.id)
+        targetMember.ban()
+        message.channel.send(`${tag} That has been banned.`)
+      }else {
+          message.channel.send(`<@${member.id} **Please specify someone to ban.**`)
+      }
+          
      } else {
          message.channel.send(`**<@${member.id}> You do not have permission to use this command.**`)
      }
