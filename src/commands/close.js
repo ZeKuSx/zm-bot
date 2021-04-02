b30fa44a077c1db37d33b68672348aa6ef7da335
ZeKuSx <81664702+ZeKuSx@users.noreply.github.com>
2021-04-02 16:01:27 +0200

    Update123123

--- src/commands/close.js
+++ src/commands/close.js
@@ -3,10 +3,9 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'close',
     maxArgs: 0,
-    permissionError: ' You must be an admin to use this command.',
-    permissions: 'ADMINISTRATOR',
     async execute(client, message, args, cmd, discord){
 
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You dont have permission to use \`- zm.close \`");
         if(message.channel.parent.id === '824340172442173530')
         {
 
